1c97dd2a0c6e00187857e3b6690292e950e2f0a5
Isaac Ribeiro Leao <Iribeiroleao9600@conestogac.on.ca>
2024-01-22 20:25:15 -0500

    Added packet class with a few comments on what could be added

---
+++ Packet.h
@@ -0,0 +1,16 @@
+#ifndef PACKET_H
+#define PACKET 
+
+class Packet {
+
+	// packet header 
+	// packet payload 
+public: 
+	Packet(); // constructor 
+
+	// writing/reading methods (or use the files you created) 
+};
+
+
+
+#endif // !PACKET_H
