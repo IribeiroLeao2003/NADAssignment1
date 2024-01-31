cd35fd9581ee5d35bf1658365dc9b6c4b2f77ebc
Isaac Ribeiro Leao <Iribeiroleao9600@conestogac.on.ca>
2024-01-31 17:26:45 -0500

    added CRC.h and pseudocode to checksum.cpp

--- checksum.cpp
+++ checksum.cpp
@@ -1,11 +1,36 @@
 //https://github.com/d-bahr/CRCpp
 
 #include "Packets.h"
-#include ""
+#include "CRC.h"
+
 
 
 string generateChecksum(string fileName)
 {
+
+	//Open file binary 
+	//if File isnt open 
+	//			Give error message
+	//			return empty string
+
+	// Declare a buffer (unsigned char)
+
+	//initialize crc
+
+	// While not at the end of the file
+	//		Read some data into the buffer
+	//		update crc with the buffer
+
+
+	// Finalize CRC (can depend on which CRC we are using)
+	
+	
+	
+	// Declare checksum as a string or char * (we will have to check on that)
+	// convert src to hexadecimal and store inside checksum
+
+
+	//return checksum
 
 
 	return "";
